7251db66bccc75d4b46eb0bc2a7c54cc0d9bf4ad
Anshul Shukla <its.anshul.shukla@gmail.com>
2026-09-12 23:33:22 +0530

    Practice exercise

---
+++ Pratice-Exercise-2-js/script.js
@@ -0,0 +1,7 @@
+const myName='Anshul';
+        const GSTpercentage=18;
+        document.querySelector('#name-display').
+        innerText=myName;
+        let cartValue=5500-5500*GSTpercentage/100+3000-3000*GSTpercentage/100;
+        console.log(cartValue);
+        console.log(eval('5+4+3+2+1;'));
